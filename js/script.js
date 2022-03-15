279c77599eaa5c251d1cc3cfca036cbdfba4ab0b
Lukasz43 <78601431+Lukasz43@users.noreply.github.com>
2022-03-15 23:16:39 +0100

    15.03.2022 INIT #2

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
 "use strict";
 
 const buttonMain = document.querySelector(".button-main");
-const lukaszH1 = document.querySelector(".lukasz-h1");
+const lukaszH1 = document.querySelector(".name");
+const scrollTop = document.querySelector(".scrollTop");
+
+let intViewportHeight = window.innerHeight;
+
 let bodyBackground = document.body;
 
 buttonMain.addEventListener("click", function () {
-  const randomHex = Math.floor(Math.random() * 16777215).toString(16);
-  console.log(randomHex);
-  document.body.style.backgroundColor = "#" + randomHex;
+  // const randomHex = Math.floor(Math.random() * 16777215).toString(16);
+  // console.log(randomHex);
+  // document.body.style.backgroundColor = "#" + randomHex;
+  window.scrollBy(0, intViewportHeight);
+  console.log("1");
+  console.log();
+});
+
+scrollTop.addEventListener("click", function () {
+  window.scrollTo(0, 0);
+  console.log("scroll top");
 });
